5aa283af29ef53db4c3be1284b661f1ce9c6e01a
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-20 14:32:37 +0200

    [add] function for view the num generate on html

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Definisco le constanti "randomNumbers" e "userNumbers" gestire i numeri casuali e quelli inseriti dall'utente.
 - Faccio il pulsante "Inizia il gioco" e aggiungo un event listener al pulsante per gestire il clic.
 - Faccio una funzione che generi 5 numeri casuali unici e li assegno all'array "randomNumbers".
+- Creo una funzione per visualizzare i numeri generati sulla pagina Html.
--- script.js
+++ script.js
@@ -19,15 +19,32 @@ function startGame() {
     
     // Genera 5 numeri casuali e li assegna all'array randomNumbers.
     randomNumbers = generateRandomNumbers(5);
-    
+
     // Mostra i numeri generati sulla pagina.
     displayNumbers(randomNumbers);
-    
+
     // Avvia un timer per nascondere i numeri dopo 5 secondi.
     setTimeout(() => {
-        clearNumbers();
-        
-        // Avvia un altro timer per chiedere all'utente di inserire i numeri.
-        setTimeout(promptUserNumbers, 100);
     }, 5000);
 }
+
+
+// Funzione per generare un array di numeri casuali unici.
+function generateRandomNumbers(count) {
+    const numbers = [];
+    while (numbers.length < count) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+    return numbers;
+}
+
+
+// Funzione per visualizzare i numeri sulla pagina HTML.
+function displayNumbers(numbers) {
+    const numbersDiv = document.getElementById('numbers');
+    numbersDiv.innerHTML = numbers.join(' ');
+}
+
