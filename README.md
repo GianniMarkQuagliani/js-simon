7c8bab0041f80573f20a149492aa1029fc3f4fd1
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-20 14:18:27 +0200

    [add] event listener click

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Simon Says
 - Creo una struttura Html e Css
 - Definisco le constanti randomNumbers e gestire i numeri casuali e quelli inseriti dall'utente.
+- Faccio il pulsante "Inizia il gioco" e aggiungo un event listener al pulsante per gestire il clic.
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // Definisco le constanti randomNumbers e gestire i numeri casuali e quelli inseriti dall'utente.
 let randomNumbers = [];
 let userNumbers = [];
+
+// Attendo il caricamento del documento HTML prima di associare eventi.
+document.addEventListener("DOMContentLoaded", function() {
+    // Faccio il pulsante "Inizia il gioco".
+    const startButton = document.getElementById("startButton");
+    // Aggiungo un event listener al pulsante per gestire il clic.
+    startButton.addEventListener("click", startGame);
+});
